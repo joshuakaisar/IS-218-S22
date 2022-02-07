4ce5e5bc749299dc54c720c16685aba1dd475dcf
joshuakaisar <kaisarjoshua@gmail.com>
2022-02-07 17:29:53 -0500

    changed name in the controller

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Keith Williams"
+        name = "Joshua Kaisar IS 218-006"
         return render_template('index.html', name=name)
